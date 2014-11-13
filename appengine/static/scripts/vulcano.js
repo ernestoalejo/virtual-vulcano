bf1a9e3879de362f1dbdfaa97a1055339c8017c4
Santiago Puerta <elwarxy@gmail.com>
2014-11-13 14:06:50 +0100

    Script en angular para generar un fichero de cloud config.

--- appengine/static/scripts/vulcano.js
+++ appengine/static/scripts/vulcano.js
@@ -1,2 +1,23 @@
 'use strict';
 
+
+var app = angular.module('app', [
+	'ng',
+]);
+
+
+app.controller('vulcano.GenerateCtrl', function ($http, $q) {
+	var ctrl = this;
+
+	ctrl.generate = function () {
+		$q.all([
+			$http.get('/static/assets/cloud-config.tmpl'),
+			// $http.get('/api/code'),
+		]).then(function (result) {
+			var template = result[0];
+			var code = result[1];
+
+			// console.log(template.data);
+		});
+	};
+});
