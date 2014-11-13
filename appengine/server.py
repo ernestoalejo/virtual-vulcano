6023ecfa7415fa961ca170267923a9fb9f47ef72
Santiago Puerta <elwarxy@gmail.com>
2014-11-13 10:52:17 +0100

    Depuración automatica en modo desarrollo y no en producción.

--- appengine/server.py
+++ appengine/server.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 import webapp2
 
+debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 application = webapp2.WSGIApplication([
   ('/', 'virtual.handlers.hello_world_handler.HelloWorldHandler'),
-], debug=True)
+], debug=debug)
