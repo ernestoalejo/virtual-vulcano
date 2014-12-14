26423c93b64e4e3685a335e18d4601dbce834dec
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2014-12-14 12:19:23 +0100

    Rename test files in the gulpfile too. Fixes #13.

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -14,6 +14,6 @@ gulp.task('default', function () {
 
 
 gulp.task('test', function () {
-    return gulp.src('lib/**/*test.js')
+    return gulp.src('lib/**/*.spec.js')
         .pipe($.jasmine());
 });
