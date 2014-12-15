c83a752fd9fc957448731f65420773d16bc88fe1
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2014-12-15 09:23:03 +0100

    Remove machines.

--- web/app/index.js
+++ web/app/index.js
@@ -8,6 +8,7 @@ var express = require('express');
 
 var app = express();
 
+
 app.get('/clusters', clusters.list);
 app.get('/clusters/:id', clusters.show);
 app.delete('/clusters/:id', clusters.destroy);
@@ -15,12 +16,7 @@ app.get('/clusters', clusters.create);
 app.put('/clusters/:id', clusters.update);
 
 
-app.get('/', function (req, res) {
-  res.send('Hello Worl2d!')
-})
-
-
 app.listen(8080, function () {
-  console.log('server listening in localhost:8080');
+  console.log('server listening in http://localhost:8080');
 });
 
