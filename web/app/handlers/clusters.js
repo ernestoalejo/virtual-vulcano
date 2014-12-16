6ae85922eb6e0f1e96efdd03dec9be1da2411ca0
Santiago Puerta <elwarxy@gmail.com>
2014-12-16 12:58:18 +0100

    Fix spacing.

--- web/app/handlers/clusters.js
+++ web/app/handlers/clusters.js
@@ -1,37 +1,35 @@
-//Copyright (c) 2014 The Virtual Vulcano authors. All rights reserved.
-//Use of this source code is governed by a BSD-style license that
-//can be found in the LICENSE.md file.
+// Copyright (c) 2014 The Virtual Vulcano authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE.md file.
 
 'use strict';
 
 var Cluster = require('../models/cluster'),
     Q = require('q');
 
+
 module.exports = {
 
-	list: function (req) {
+	list: function (req, res) {
 		return Q.nfcall(Cluster.find, {});
 	},
 
-	show: function (req) {
+	show: function (req, res) {
 		return Q.nfcall(Cluster.findById(req.id));
 	},
 
-	destroy: function (req) {
+	destroy: function (req, res) {
 		return Q.nfcall(Cluster.remove({_id: req.id}));	
 	},
 
-	create:function (req) {
-		Cluster.clusterId:req.id;
-		Cluster.name:req.name;
-		Cluster.createdAt:req.createdAt;
-		Cluster.updatedAt:req.updatedAt;
-		
+	create: function (req, res) {
+		Cluster.clusterId = req.id;
+		Cluster.name = req.name;
+		Cluster.createdAt = req.createdAt;
+		Cluster.updatedAt = req.updatedAt;
 	},
 
 	update: function (req, res) {
 	},
 
 };
-
-module.exports.list('tt','tt');
