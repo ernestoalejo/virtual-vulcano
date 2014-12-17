daf1b1b3e257ac33ea06e46c14c45e0e2dd58144
Alberto Moreno <albertinivva@gmail.com>
2014-12-17 17:39:32 +0100

    Be consistent with the rest of files

--- web/app/lib/start.js
+++ web/app/lib/start.js
@@ -1,8 +1,8 @@
-'use strict';
-
 // Copyright (c) 2014 The Virtual Vulcano authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE.md file.
+
+'use strict';
 
 var crypto = require('crypto'),
     childProccessPromise = require('child-process-promise');
