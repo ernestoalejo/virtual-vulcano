8c8985f4b16e4c33ab8609c3ec3e8603cafaf872
yonatan24891 <yonatan24891@hotmail.com>
2014-12-17 17:45:16 +0100

    Be consistent with the rest of files

--- web/app/assets/start.tmpl.sh
+++ web/app/assets/start.tmpl.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 # Copyright (c) 2014 The Virtual Vulcano authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that
 # can be found in the LICENSE.md file.
 
-#!/bin/bash
 
 echo 'Hello world!'
