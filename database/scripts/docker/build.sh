96831bb8b4b379c27cb38968345a35c67c1a9001
Santiago Puerta <elwarxy@gmail.com>
2015-01-15 13:55:20 +0100

    Add scripts and basic configuration for the DB container.

---
+++ database/scripts/docker/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copyright (c) 2015 The Virtual Vulcano authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that
+# can be found in the LICENSE.md file.
+
+set -e
+set -u
+
+docker build -t virtualvulcano/database .
