8a652606ce144abf1836324929a025f401950264
Alberto Moreno <albertinivva@gmail.com>
2015-01-15 10:42:21 +0100

    Delete old code.

--- web/app/index.js
+++ web/app/index.js
@@ -7,13 +7,11 @@
 require('./models/database');
 
 var express = require('express'),
-    clusters = require('./handlers/clusters'),
     base = require('./handlers/base'),
     ejs = require('ejs'),
     path = require('path'),
     promised = require('./middlewares/promised'),
-    bodyParser = require('body-parser'),
-    sshConfig = require('./lib/ssh-config');
+    bodyParser = require('body-parser');
 
 
 var app = express();
@@ -30,23 +28,11 @@ app.set('view engine', 'html');
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 
-// Handlers
-app.delete('/api/v1/clusters/:id', promised(clusters.destroy));
-app.get('/api/v1/clusters', promised(clusters.list));
-app.get('/api/v1/clusters/:id', promised(clusters.show));
-app.get('/api/v1/clusters/:id/install', promised(clusters.install));
-app.post('/api/v1/clusters', promised(clusters.create));
-app.post('/api/v1/clusters/:id/run', promised(clusters.run));
-app.put('/api/v1/clusters/:id', promised(clusters.update));
-
+// Base handler
 app.get('/', base.base);
 app.get('/:s1', base.base);
 app.get('/:s1/:s2', base.base);
 app.get('/:s1/:s2/:s3', base.base);
-
-
-// Config SSH settings on startup
-sshConfig.startup().done();
 
 
 // Start server
