d18dff412d2964865f62d5fcf1cd81b5322cc2ec
yonatan24891 <yonatan24891@hotmail.com>
2015-01-15 10:31:16 +0100

    Folder monitor deleted

--- monitor/docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) 2014 The Virtual Vulcano authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that
-# can be found in the LICENSE.md file.
-# 
-
-docker build -t virtualvulcano/monitor .
