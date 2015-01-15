b4df84b083eab8cc87c70e55c6dc77a2c51b9abf
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-01-15 10:32:57 +0100

    Move docker scripts to a subfolder.

--- web/scripts/docker/run.sh
+++ web/scripts/docker/run.sh
@@ -7,4 +7,4 @@
 set -e
 set -u
 
-docker run --privileged=true -v $(pwd):/web -i -p 8080:8080 -t virtualvulcano/web /web/cmd/local.sh
+docker run --privileged=true -v $(pwd):/web -i -p 8080:8080 -t virtualvulcano/web bash
