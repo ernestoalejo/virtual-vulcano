761b4f23ae102c8007880a834f9445db4220065f
Santiago Puerta <elwarxy@gmail.com>
2015-01-21 19:58:10 +0100

    Add logging statements.

--- scripts/install/run.sh
+++ scripts/install/run.sh
@@ -7,20 +7,26 @@ set -eu
 
 SERVICES_PATH=/opt/virtualvulcano/services
 
+echo " [*] Cache web server..."
 docker pull virtualvulcano/web-server
 
+echo " [*] Prepare dist folder..."
 sudo mkdir -p $SERVICES_PATH
 cd $SERVICES_PATH
 
+echo " [*] Download services files..."
 curl https://raw.githubusercontent.com/ernestoalejo/virtual-vulcano/master/services/database.service | sudo tee database.service > /dev/null
 curl https://raw.githubusercontent.com/ernestoalejo/virtual-vulcano/master/services/web.service | sudo tee web.service > /dev/null
 curl https://raw.githubusercontent.com/ernestoalejo/virtual-vulcano/master/services/ftp.service | sudo tee ftp.service > /dev/null
 
+echo " [*] Enable & start database service..."
 sudo systemctl enable $SERVICES_PATH/database.service
 sudo systemctl start database
 
+echo " [*] Enable & start web service..."
 sudo systemctl enable $SERVICES_PATH/web.service
 sudo systemctl start web
 
+echo " [*] Enable & start ftp service..."
 sudo systemctl enable $SERVICES_PATH/ftp.service
 sudo systemctl start ftp
