f2d3d0beaeb471bf9c0e1e574aa8df99f52511ca
Santiago Puerta <elwarxy@gmail.com>
2015-01-21 18:15:56 +0100

    Fix the fleetctl start problem.

--- scripts/install/run.sh
+++ scripts/install/run.sh
@@ -15,10 +15,12 @@ cd $SERVICES_PATH
 wget https://raw.githubusercontent.com/ernestoalejo/virtual-vulcano/master/services/database.service
 wget https://raw.githubusercontent.com/ernestoalejo/virtual-vulcano/master/services/web.service
 
-fleetctl submit database.service
-fleetctl load database.service
-fleetctl start database.service
+sudo systemctl enable database.service
+sudo systemctl start database.service
 
-fleetctl submit web.service
-fleetctl load web.service
-fleetctl start web.service
+sudo systemctl enable web.service
+sudo systemctl start web.service
+
+sudo systemctl enable ftp.service
+sudo systemctl start ftp.service
+
