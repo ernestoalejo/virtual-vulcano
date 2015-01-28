0ecc5dabe1fbfcdaa72c883ee8ebc47f6d18b46a
Alberto Moreno <albertinivva@gmail.com>
2015-01-28 19:52:34 +0100

    Add logging to distinct the different runs of the web container.

---
+++ web/scripts/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copyright (c) 2015 The Virtual Vulcano authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that
+# can be found in the LICENSE.md file.
+
+
+echo
+echo " ======================================================================"
+echo "   New container at: $(date)"
+echo " ======================================================================"
+echo
+
+node app/seed.js
+node app/index.js
