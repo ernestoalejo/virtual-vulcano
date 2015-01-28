0cefc1a5008adcfaa64916a3bdeb48e824282ae4
Alberto Moreno <albertinivva@gmail.com>
2015-01-28 19:30:41 +0100

    Add plugins definitions.

---
+++ web/app/plugins/monitoring.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2015 The Virtual Vulcano authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE.md file.
+
+'use strict';
+
+var plugins = require('../lib/plugins');
+
+
+plugins.register({
+  name: 'Monitoring',
+  url:  '/acounts/monitoring',
+  icon: 'fa-tachometer',
+});
