76a33b65801dc12427961be8f510ced21acf7722
yonatan24891 <yonatan24891@hotmail.com>
2015-02-02 17:55:32 +0100

    Add lincenses

--- scripts/push/database.sh
+++ scripts/push/database.sh
@@ -1,3 +1,7 @@
+#Copyright (c) 2015 The Virtual Vulcano authors. All rights reserved.
+#Use of this source code is governed by a BSD-style license that
+#can be found in the LICENSE.md file.
+
 #!/bin/bash
 
 docker build -t virtualvulcano/database database
