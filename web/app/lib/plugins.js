12071960a60c6c054fd0c6072577435ef5e051dd
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-02-02 20:35:24 +0100

    Add comments.

--- web/app/lib/plugins.js
+++ web/app/lib/plugins.js
@@ -10,10 +10,23 @@ var plugins = [];
 
 module.exports = {
 
+  /**
+   * Register a new plugin in the system.
+   * @param {Object} info The info about the plugin. It should have these keys:
+   *   - name <string> The name that will be showed in the dashboard.
+   *   - url <string> The URL of the link in the dashboard.
+   *   - icon <string> The name of the Font Awesome icon for the dashboard.
+   *   - routes <function(app)> Function that will be called to register the
+   *       routes this plugin will handle.
+   */
   register: function (info) {
     plugins.push(info);
   },
 
+  /**
+   * List the registered plugins.
+   * @return {Array} The list of plugins.
+   */
   list: function () {
     return plugins;
   },
