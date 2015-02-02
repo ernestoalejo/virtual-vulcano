a0d79d40775555ea1cb1c1a4a92f6244c1769429
yonatan24891 <yonatan24891@hotmail.com>
2015-02-02 21:35:31 +0100

    Test plugins lib

---
+++ web/app/lib/plugins.test.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2015 The Virtual Vulcano authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE.md file.
+
+'use strict';
+
+var plugins = require('./plugins');
+
+
+describe('plugins register', function () {
+
+  it('should return an empty list', function () {
+    expect(plugins.list()).toEqual([]);
+  });
+
+  it('should store the plugins list', function () {
+    plugins.register('foo');
+    expect(plugins.list()).toEqual(['foo']);
+  });
+
+});
