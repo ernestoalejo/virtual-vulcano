36b14747524fbb4bc64486385c2cca15339fa788
Santiago Puerta <elwarxy@gmail.com>
2015-02-10 13:05:38 +0100

    Redirect root to phpMyAdmin. Fix #68

---
+++ phpmyadmin/provision/index.php
@@ -0,0 +1,8 @@
+<?php
+// Copyright (c) 2015 The Virtual Vulcano authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE.md file.
+
+header('Location: /phpmyadmin/index.php');
+
+?>
